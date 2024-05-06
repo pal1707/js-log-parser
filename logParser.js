4934305a86c0e402f09145319dfe107655ce14c5
pal.mashruwala@digitality.com.au <pal.mashruwala@digitality.com.au>
2024-05-06 11:34:48 +1000

    Modify the code to handle the large log file + commenting on the code + Test case changes

--- logParser.js
+++ logParser.js
@@ -1,68 +1,88 @@
 // logParser.js
 
+const fs = require('fs');
+const readline = require('readline');
+
 class LogParser {
-    constructor(logData) {
-      this.logData = logData;
-    }
-  
-    // Parses the log data and returns an array of objects containing IP addresses and URLs
-    parseLog() {
-      const lines = this.logData.split('\n');
-      const parsedData = [];
-  
-      lines.forEach(line => {
-        const parts = line.split(' ');
-        const ip = parts[0];
-        const url = parts[6];
-        parsedData.push({ ip, url });
-      });
-  
-      return parsedData;
-    }
-  
-    // Returns the number of unique IP addresses in the log data
-    getUniqueIPCount() {
-      const parsedData = this.parseLog();
-      const uniqueIPs = new Set(parsedData.map(entry => entry.ip));
-      return uniqueIPs.size;
-    }
-  
-    // Returns an array of the top 3 most visited URLs in the log data
-    getTopVisitedURLs() {
-      const parsedData = this.parseLog();
-      const urlCount = {};
-  
-      parsedData.forEach(entry => {
-        const { url } = entry;
-        if (urlCount[url]) {
-          urlCount[url]++;
-        } else {
-          urlCount[url] = 1;
-        }
-      });
-  
-      const sortedURLs = Object.keys(urlCount).sort((a, b) => urlCount[b] - urlCount[a]);
-      return sortedURLs.slice(0, 3);
-    }
-  
-    // Returns an array of the top 3 most active IP addresses in the log data
-    getTopActiveIPs() {
-      const parsedData = this.parseLog();
-      const ipCount = {};
-  
-      parsedData.forEach(entry => {
-        const { ip } = entry;
-        if (ipCount[ip]) {
-          ipCount[ip]++;
-        } else {
-          ipCount[ip] = 1;
-        }
-      });
-  
-      const sortedIPs = Object.keys(ipCount).sort((a, b) => ipCount[b] - ipCount[a]);
-      return sortedIPs.slice(0, 3);
+  constructor(logFilePath) {
+    this.logFilePath = logFilePath;
+  }
+
+  async getUniqueIPCount() {
+    // Using a Set to store unique IP addresses
+    const uniqueIPs = new Set();
+
+    // Asynchronously process the log file line by line
+    await this.processLogFile(line => {
+      // Extracting IP address from each line and adding it to the set
+      const ip = line.split(' ')[0];
+      uniqueIPs.add(ip);
+    });
+
+    // Returning the size of the set, which is the count of unique IP addresses
+    return uniqueIPs.size;
+  }
+
+  async getTopVisitedURLs() {
+    // Using a Map to store URLs and their visit counts
+    const urlMap = new Map();
+
+    // Asynchronously process the log file line by line
+    await this.processLogFile(line => {
+      // Extracting URL from each line and adding it to the map
+      const url = line.split('"')[1].split(' ')[1];
+      const count = urlMap.get(url) || 0;
+      urlMap.set(url, count + 1);
+    });
+
+    // Returning the top 3 visited URLs
+    return this.getTopItems(urlMap, 3);
+  }
+
+  async getTopActiveIPs() {
+    // Using a Map to store IP addresses and their activity counts
+    const ipMap = new Map();
+
+    // Asynchronously process the log file line by line
+    await this.processLogFile(line => {
+      // Extracting IP address from each line and adding it to the map
+      const ip = line.split(' ')[0];
+      const count = ipMap.get(ip) || 0;
+      ipMap.set(ip, count + 1);
+    });
+
+    // Returning the top 3 active IP addresses
+    return this.getTopItems(ipMap, 3);
+  }
+
+    //Here, we're using fs.createReadStream() along with the readline module to read the log file line by line. 
+  //This way, we don't load the entire file into memory, making it efficient for extremely large log files.
+  async processLogFile(callback) {
+    // Create a readable stream from the log file
+    const inputStream = fs.createReadStream(this.logFilePath);
+
+    // Create a readline interface to read the stream line by line
+    const rl = readline.createInterface({
+      input: inputStream,
+      crlfDelay: Infinity // Detecting line breaks correctly
+    });
+
+    // Iterate over each line in the log file
+    for await (const line of rl) {
+      // Call the callback function with each line
+      callback(line);
     }
   }
-  
-  module.exports = LogParser;
-  
+
+  // Utility function to get the top items from a Map
+  getTopItems(map, count) {
+    return [...map.entries()]
+      .sort((a, b) => b[1] - a[1]) // Sort by value (count) in descending order
+      .slice(0, count) // Take the top 'count' items
+      .map(([key]) => key); // Extract only the keys (URLs or IP addresses)
+  }
+}
+
+module.exports = LogParser;
+
+
--- logParser.test.js
+++ logParser.test.js
@@ -1,28 +1,31 @@
 // logParser.test.js
 
-const fs = require('fs');
 const LogParser = require('./logParser');
 
-// Read the log data from the log.txt file
-const exampleData = fs.readFileSync('./log.txt', 'utf8');
+describe('LogParser', () => {
+  const logFilePath = './log.txt';
+  const logParser = new LogParser(logFilePath);
 
-// Test case to check the getUniqueIPCount method
-test('Test getUniqueIPCount', () => {
-  const logParser = new LogParser(exampleData);
-  // The number of unique IP addresses in the log data should be 10
-  expect(logParser.getUniqueIPCount()).toBe(10);
-});
+  test('getUniqueIPCount', async () => {
+    const uniqueIPCount = await logParser.getUniqueIPCount();
+    expect(uniqueIPCount).toBe(11);
+  });
 
-// Test case to check the getTopVisitedURLs method
-test('Test getTopVisitedURLs', () => {
-  const logParser = new LogParser(exampleData);
-  // The top 3 most visited URLs in the log data should be '/docs/manage-websites/', 'http://example.net/faq/', and '/intranet-analytics/'
-  expect(logParser.getTopVisitedURLs()).toEqual(['/docs/manage-websites/', 'http://example.net/faq/', '/intranet-analytics/']);
-});
+  test('getTopVisitedURLs', async () => {
+    const topVisitedURLs = await logParser.getTopVisitedURLs();
+    expect(topVisitedURLs).toEqual([
+      '/intranet-analytics/',
+      'http://example.net/faq/',
+      '/this/page/does/not/exist/'
+    ]);
+  });
 
-// Test case to check the getTopActiveIPs method
-test('Test getTopActiveIPs', () => {
-  const logParser = new LogParser(exampleData);
-  // The top 3 most active IP addresses in the log data should be '168.41.191.40', '177.71.128.21', and '50.112.00.11'
-  expect(logParser.getTopActiveIPs()).toEqual(['168.41.191.40', '177.71.128.21', '50.112.00.11']);
+  test('getTopActiveIPs', async () => {
+    const topActiveIPs = await logParser.getTopActiveIPs();
+    expect(topActiveIPs).toEqual([
+      '168.41.191.40',
+      '177.71.128.21',
+      '50.112.00.11'
+    ]);
+  });
 });
